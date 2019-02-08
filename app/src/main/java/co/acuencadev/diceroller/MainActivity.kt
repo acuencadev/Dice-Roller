953e4b7cfc1a6805d0195cfb89c25def277ac01e
Amador Cuenca <sphi02ac@gmail.com>
2019-02-08 09:25:32 -0500

    feat: Handle OnClick on the Roll button

--- app/src/main/java/co/acuencadev/diceroller/MainActivity.kt
+++ app/src/main/java/co/acuencadev/diceroller/MainActivity.kt
@@ -2,11 +2,19 @@ package co.acuencadev.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's Roll"
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
